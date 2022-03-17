93800d5662fdb05f6e41bb6a059b803667a6a99c
Tanishq Kashyap <tanishqa89@gmail.com>
2022-03-18 01:46:03 +0530

    login using otp page added

---
+++ lib/pages/login_using_otp.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:sehyog/pages/opt_verification.dart';
+
+import '../common/theme_helper.dart';
+import 'login_page.dart';
+import 'widgets/header_widget.dart';
+
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({Key? key}) : super(key: key);
+
+  @override
+  _ForgotPasswordPageState createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final _formKey = GlobalKey<FormState>();
+  String aadhaarCard = "";
+
+  @override
+  Widget build(BuildContext context) {
+    double _headerHeight = 300;
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: _headerHeight,
+                child: HeaderWidget(
+                    _headerHeight, true, 'assets/images/sehyog_short.png'),
+              ),
+              SafeArea(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.topLeft,
+                        margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Login with OTP',
+                              style: TextStyle(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black54),
+                              // textAlign: TextAlign.center,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'Enter the aadhaar number associated with your '
+                              'account.',
+                              style: TextStyle(
+                                // fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black54,
+                              ),
+                              // textAlign: TextAlign.center,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'We will send you a verification code on your '
+                              'mobile number linked with your aadhaar card ',
+                              style: TextStyle(
+                                color: Colors.black38,
+                                // fontSize: 20,
+                              ),
+                              // textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 40.0),
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              child: TextFormField(
+                                keyboardType: TextInputType.number,
+                                decoration: ThemeHelper().textInputDecoration(
+                                    "Aadhaar Number",
+                                    "Enter your aadhaar "
+                                        "number"),
+                                validator: (val) {
+                                  if (val!.isEmpty) {
+                                    return "Aadhar number can't be empty";
+                                  }
+                                  aadhaarCard = val;
+
+                                  return null;
+                                },
+                              ),
+                              decoration:
+                                  ThemeHelper().inputBoxDecorationShaddow(),
+                            ),
+                            SizedBox(height: 40.0),
+                            Container(
+                              decoration:
+                                  ThemeHelper().buttonBoxDecoration(context),
+                              child: ElevatedButton(
+                                style: ThemeHelper().buttonStyle(),
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(40, 10, 40, 10),
+                                  child: Text(
+                                    "Send".toUpperCase(),
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              ForgotPasswordVerificationPage(
+                                                  aadhaarCard)),
+                                    );
+                                  }
+                                },
+                              ),
+                            ),
+                            SizedBox(height: 30.0),
+                            Text.rich(
+                              TextSpan(
+                                children: [
+                                  TextSpan(text: "Remember your password? "),
+                                  TextSpan(
+                                    text: 'Login',
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  LoginPage()),
+                                        );
+                                      },
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
